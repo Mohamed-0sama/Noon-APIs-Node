e050987a8696bc99eeb5a438c5982f8836a6a152
Mohamed Osama <mohamed.osama.swe@gmail.com>
2021-11-18 16:48:58 +0200

    adjust token

--- middlewares/Authorization.js
+++ middlewares/Authorization.js
@@ -4,6 +4,7 @@ const verifyToken = (req, res, next) => {
   const authHeader = req.headers.token;
   if (authHeader) {
     // const token = authHeader.split(" ")[1];
+    const token = authHeader;
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
       if (err) res.status(403).json("Token is not valid!");
       req.user = user;
