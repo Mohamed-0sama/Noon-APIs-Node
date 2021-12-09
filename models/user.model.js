25bde313c8208e851fb6f6b618d390c490c12c32
Mohamed Osama <mohamed.osama.swe@gmail.com>
2021-12-09 18:45:08 +0200

    add default mages

--- models/user.model.js
+++ models/user.model.js
@@ -5,7 +5,7 @@ const UserSchema = new mongoose.Schema(
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    imageSrc: { type: String },
+    imageSrc: { type: String, default: "defaultprofile.png" },
     isAdmin: {
       type: Boolean,
       default: false,
